56f1dcea96ac9aee3e50331a065814006b96e081
Gustavo <gstonin95@gmail.com>
2020-10-03 06:07:09 +0100

    Updates

---
+++ precache-manifest.fd9938cbdd5d2faf16515d6ba61f3801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d52daa0b4e0bc74c1c81ac5db480be6e",
+    "url": "/small-library-react/index.html"
+  },
+  {
+    "revision": "c393b3441bc4e9054a4f",
+    "url": "/small-library-react/static/css/main.056eebe7.chunk.css"
+  },
+  {
+    "revision": "700854b734c86947f8e4",
+    "url": "/small-library-react/static/js/2.47200757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/small-library-react/static/js/2.47200757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c393b3441bc4e9054a4f",
+    "url": "/small-library-react/static/js/main.ff48ae71.chunk.js"
+  },
+  {
+    "revision": "59ea65dc24cfa4086d25",
+    "url": "/small-library-react/static/js/runtime-main.87c9ae7e.js"
+  },
+  {
+    "revision": "02e85c278e276802a0406e11287a976a",
+    "url": "/small-library-react/static/media/github.02e85c27.png"
+  }
+]);
